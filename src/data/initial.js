a89fa605095de46877e25448a8e0c6a4682cc1d5
Tumeka Burston <burston1993@icloud.com>
2021-01-05 00:12:37 +0000

    initial.js set up

---
+++ src/data/initial.js
@@ -0,0 +1,13 @@
+const initial = {
+    team1Name: "",
+    team2Name: "",
+    team1Players: [],
+    team2Players: [],
+    team1Colour: "",
+    team2Colour: "",
+    allPlayers: [],
+    team1Score: 0,
+    team2Score: 0,
+    teamsConfirmed: false,
+    playerAmount: 5,
+}
