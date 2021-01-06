dbc881aea35d3982e9d7947df56f0d53577759f8
Tumeka Burston <burston1993@icloud.com>
2021-01-06 21:14:36 +0000

    added target=blank to footer github link

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import github from '../assets/icons/github.svg';
 
 const footer = () => (
     <footer>
-        <a href="https://github.com/Tumekab/tech-challenge"><img className="github" src={github} alt="github link"/></a>
+        <a href="https://github.com/Tumekab/tech-challenge" target="_blank"><img className="github" src={github} alt="github link"/></a>
     </footer>
 )
 
