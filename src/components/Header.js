3ab51942f1adff7955dcddb8a959e67b7c3e3341
Tumeka Burston <burston1993@icloud.com>
2021-01-06 22:57:12 +0000

    changed url in package json

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => (
     <>
         <header className="headerBG">
-            <h1 className="title"><a href="/tech-challenge">it's coming home</a></h1>
+            <h1 className="title"><a href="/#/">it's coming home</a></h1>
         </header>
         <h3 className="subTitle">a 5-a-side team generator</h3>
     </>
